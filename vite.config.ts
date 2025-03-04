e71cef1126b95c382158f3d4feede38919223e89
Akshay Chandrasekhar <akschand24@gmail.com>
2025-03-03 19:20:45 -0500

    Update vite.config.ts 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,36 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
+import path, { dirname } from "path";
+import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { fileURLToPath } from "url";
 
-// https://vitejs.dev/config/
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export default defineConfig({
   base: '/algorhythmandflow/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    runtimeErrorOverlay(),
+    themePlugin(),
+    ...(process.env.NODE_ENV !== "production" &&
+    process.env.REPL_ID !== undefined
+      ? [
+          await import("@replit/vite-plugin-cartographer").then((m) =>
+            m.cartographer(),
+          ),
+        ]
+      : []),
+  ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'client', 'src'),
-      '@shared': path.resolve(__dirname, 'shared'),
+      "@": path.resolve(__dirname, "client", "src"),
+      "@shared": path.resolve(__dirname, "shared"),
     },
   },
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: false,
   },
-})
+});
